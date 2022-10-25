b938f7ec49e4fe882c515414391b21096092f50d
Wood <n01368202@unf.edu>
2022-10-25 10:40:34 -0400

    Project creation

---
+++ SportsStore/Models/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SportsStore.Models
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
