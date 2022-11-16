27702c564278a50a875951119519d40f53707ce1
Nate <natewood808@gmail.com>
2022-11-16 17:18:13 -0500

    Added to the NavigationMenuViewComponent to return a Default.cshtml view that renders the Category list and necessary HTML.

--- SportsStore/Models/ViewModels/ProductsListViewModel.cs
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -2,14 +2,18 @@
 
 namespace SportsStore.Models.ViewModels
 {
-    // This class is used to wrap all the data we are going to send
-    // from the controller to the view.
-
-    // We update the List action method in ProductController.cs
-    // to use this class to provide the view with details of the
-    // products to display on the page and details of the pagination.
-
-    // Updated to include the current category being filtered by, if any.
+    /// This class is used to wrap all the data we are going to send
+    /// from the controller to the view.
+    
+    /// We update the List action method in ProductController.cs
+    /// to use this class to provide the view with details of the
+    /// products to display on the page and details of the pagination.
+    
+    /// Updated to include the current category being filtered by, if any.
+     
+    /// Careful not to extend view models with copious amounts of data as
+    /// every action method that uses the view model will need to be updated
+    /// and the purpose of action methods may become unclear.
     public class ProductsListViewModel
     {
         public IEnumerable<Product> Products { get; set; }
