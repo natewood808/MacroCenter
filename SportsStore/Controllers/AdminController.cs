51fee4c1ce706d0ed4ef44c37aba3195e809f3e7
Wood <n01368202@unf.edu>
2022-11-17 15:43:37 -0500

    Created the AdminController and the Index.cshtml view for the controller. This view lists all the products available in the database and provides buttons that will call action methods for Editing, Deleting, and Creating products.

---
+++ SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        /// <summary>
+        /// Calls the View method to select the default view for the action,
+        /// passing the set of products in the database as the view's model.
+        /// </summary>
+        /// <returns></returns>
+        public ViewResult Index() => View(repository.Products);
+    }
+}
