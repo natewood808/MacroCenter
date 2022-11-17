e50d70d1d1f3845b7c14d37c7ec61ecbfd7e7042
Wood <n01368202@unf.edu>
2022-11-17 15:59:16 -0500

    Created the Edit.cshtml view where the user can see the properties of a specific product and change them if needed.

--- SportsStore/Controllers/AdminController.cs
+++ SportsStore/Controllers/AdminController.cs
@@ -21,5 +21,14 @@ namespace SportsStore.Controllers
         /// </summary>
         /// <returns></returns>
         public ViewResult Index() => View(repository.Products);
+
+        /// <summary>
+        /// Finds the product that corresponds with the productId parameter
+        /// (the same product in the same row as the Edit button that was clicked),
+        /// and passes it to the Edit.cshtml view.
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns></returns>
+        public ViewResult Edit(int productId) => View(repository.Products.FirstOrDefault(p => p.ProductID == productId));
     }
 }
