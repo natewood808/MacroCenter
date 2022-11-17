0c291517fe5a26aca66c86dd37e5611610f15265
Nate <natewood808@gmail.com>
2022-11-16 20:48:51 -0500

    Creating the CartController which handles the actions for adding items and removing items from the cart. We also create extension methods in the SessionExtensions.cs class which handles serializing and deserailzing Cart objects for us to save them in ASP.NET Core's session state feature.

---
+++ SportsStore/Controllers/CartController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.AddItem(product, 1);
+                SaveCart(cart);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+            return cart;
+        }
+
+        private void SaveCart(Cart cart)
+        {
+            // The HttpContext property is provided the Controller base class,
+            // and returns an HttpContext object that provides context data about
+            // the request that has been received and the response that is being prepared.
+            // The Session property returns an object that implements the ISession interface,
+            // which is the type we defined our extension method, SetJson(this ISession session ...).
+
+            HttpContext.Session.SetJson("Cart", cart);
+        }
+    }
+}
