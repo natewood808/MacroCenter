fdb775825fc3c244afcede467cd88b6ab88c9058
Nate <natewood808@gmail.com>
2022-11-17 09:52:38 -0500

    Added the Order.cs model to hold shipping information. Created a checkout button on the Cart/Index.cshtml view which requests the Checkout action method on our new OrderController. Checkout method displays a view where users can POST shipping details.

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+
+    public class OrderController : Controller
+    {
+        /// <summary>
+        /// This class generates a default view and passes a new Order object to the view
+        /// </summary>
+        /// <returns></returns>
+        public ViewResult Checkout() => View(new Order());
+    }
+}
