372eeb346c660f8bc556fa9920b0f29c822e6e92
Nate <natewood808@gmail.com>
2022-12-12 23:33:52 -0500

    Added AccountController which handles logging in and logging out, creating new users, editing existing users, and deleting users.

---
+++ SportsStore/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models.ViewModels
+{
+    /// <summary>
+    /// This is a view model which is used to represent user login info.
+    /// </summary>
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+
+        [Required] // Uses model validation to make sure values are provided.
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        // When we use asp-for attribute on the input element in the login Razor view,
+        // the tag helper will set the type attribute to password; that way, the text
+        // entered by the user isn't visible onscreen
+        [UIHint("password")]
+        public string Password { get; set; }
+        
+        public string ReturnUrl { get; set; }
+    }
+}
